bd8a6fb460dced06dc02a9ddf6ae511f71d97cf5
acciojob-tech-team-3 <techteam+3@acciojob.com>
2025-12-29 11:21:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+Array.prototype.myMap = function(callback) {
+    
+  };
+  
+  Array.prototype.myFilter = function(callback) {
+  
+  };
+  
+  Array.prototype.myReduce = function(callback, initialValue) {
+
+  };
+  
+  
+module.exports = Array.prototype;
