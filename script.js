2f29377e60bbcba7ff82b732af3c8c5a9e60a16a
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-12-29 12:27:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
 Array.prototype.myMap = function(callback) {
-    
+    if(typeof callback!=="function"){
+		return new Error("Please provide callback")
+	}
+	if(this.length==0){
+		return []
+	}
+
+	let newArray=[]
+
+	for(let i=0;i<this.length;i++){
+		newArray.push(callback(this[i],i,this))
+	}
+
+
+	return newArray
   };
   
   Array.prototype.myFilter = function(callback) {
-  
+	  if(typeof callback!=="function"){
+		return new Error("Please provide callback")
+	  }
+	  if(this.length==0){
+		return []
+	 }
+
+	  let newArray=[]
+
+	  for(let i=0;i<this.length;i++){
+		  newArray.push(callback(this[i],i,this))
+	  }
+
+	  return newArray
   };
   
   Array.prototype.myReduce = function(callback, initialValue) {
+	  if(typeof callback!==function){
+		return new Error("Please provide callback")
+	}
+	if(this.length==0){
+		return []
+	}
 
+	  let accumulator=initialValue;
+	  let startIndex=0
+
+	  if(accumulator==undefined){
+		  accumulator=this[0]
+		  startIndex=1
+	  }
+
+	for(let i=0;i<this.length;i++){
+		accumulator=callback(accumulator,this[i],i,this)
+	}
   };
   
   
